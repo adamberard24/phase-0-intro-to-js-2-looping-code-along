c51dcc0f466c1b2744446b4d28c1663c15493cc0
Adam Schwartz <adamberard24@gmail.com>
2021-03-10 15:31:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(name, event) {
+    let buds = []
+    for (let i = 0; i < name.length; i++) {
+        buds.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    debugger
+    }
+    return buds;
+}
+
+
+function countDown() {
+    let count = 10
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
